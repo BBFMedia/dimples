eb9f1f7d311cfc2515e1ea7ac9b5989f7644d2bb
adrian <adrian@e9c906a3-4773-48ae-bc5e-25b0862e5a7a>
2011-01-14 08:10:52 +0000

    git-svn-id: http://www.solutionlenses.com/svn/bbf/Dimples_ORM@39 e9c906a3-4773-48ae-bc5e-25b0862e5a7a

---
+++ db.class.php
@@ -0,0 +1,73 @@
+<?php
+
+class db{
+
+/*** Declare instance ***/
+private static $instance = NULL;
+
+/**
+*
+* the constructor is set to private so
+* so nobody can create a new instance using new
+*
+*/
+private function __construct() {
+  /*** maybe set the db name here later ***/
+}
+
+/**
+*
+* Return DB instance or create intitial connection
+*
+* @return object (PDO)
+*
+* @access public
+*
+*/
+public static function getInstance() {
+
+
+///***  uri for the PDO connection  ***/
+
+$hosturi = DIMPLE_DB_URI; // for mysql
+// $hosturi = 'sqlite:'.$hostname ; // for sqlite
+///***  username ***/
+$username = DIMPLE_DB_USER;
+
+///***  password ***/
+$password = DIMPLE_DB_PASS;
+
+
+
+if (!self::$instance)
+    {
+    self::$instance = new PDO($hosturi, $username, $password);;
+    self::$instance-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+return self::$instance;
+}
+    /*
+     * Passes on any static calls to this class onto the singleton PDO instance
+     * @param $chrMethod, $arrArguments
+     * @return $mix
+     */
+final public static function __callStatic( $chrMethod, $arrArguments ) {
+           
+        $objInstance = self::getInstance();
+       
+        return call_user_func_array(array($objInstance, $chrMethod), $arrArguments);
+       
+    }
+/**
+*
+* Like the constructor, we make __clone private
+* so nobody can clone the instance
+*
+*/
+private function __clone(){
+}
+
+} /*** end of class ***/
+
+
+?>
